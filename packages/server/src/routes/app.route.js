ce87a73e9d34d21173656b2ab3ef8b3b7cab9d74
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2020-06-12 18:13:04 -0300

    remove: server

--- packages/server/src/routes/app.route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const UserController = require('../controllers/user.controller');
-
-const routes = Router();
-
-routes.get('/user', UserController.index);
-
-module.exports = routes;
